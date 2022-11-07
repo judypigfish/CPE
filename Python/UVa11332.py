06d3c2f3bea7397db7b3a68b89d508fba7eb1eb9
judypigfish <92738115+judypigfish@users.noreply.github.com>
2022-11-07 12:38:42 +0800

    Update UVa11332.py

--- Python/UVa11332.py
+++ Python/UVa11332.py
@@ -1,11 +1,11 @@
 while(1):
-  a = input()
-  if a=="0":
-    break
-  
-  while(len(a)>1):
-    x=0
-    for i in range(len(a)):
-      x += int(a[i])
-     a = str(x)
-  print(a)
+	a = input()
+	if a=="0":
+		break
+
+	while(len(a)>1):
+		x=0
+		for i in range(len(a)):
+			x += int(a[i])
+		a = str(x)
+	print(a)
