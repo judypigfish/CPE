76f28190f5627a0c0a4630c397f0e45d46940853
judypigfish <92738115+judypigfish@users.noreply.github.com>
2023-05-24 11:55:08 +0800

    Update UVa11349.py

--- Python/UVa11349.py
+++ Python/UVa11349.py
@@ -1,8 +1,8 @@
 num = eval(input())
 for i in range(num):
 	N = int(input().split()[2])
- 	s = []
- 	for j in range(N):
+	s = []
+	for j in range(N):
 		s += list(map(int,input().split()))
 	
 	if s == s[::-1] and min(s) >= 0:
