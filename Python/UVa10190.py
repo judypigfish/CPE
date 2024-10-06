d0a1043c706385636c244e179a3119349a8d1e64
Judy <92738115+judypigfish@users.noreply.github.com>
2024-10-06 22:18:47 +0800

    Create UVa10190.py

---
+++ Python/UVa10190.py
@@ -0,0 +1,23 @@
+while 1:
+    try:
+        a,b = list(map(int,input().split()))
+    except:
+        break
+    
+    if b == 1:
+        print("Boring!")
+        continue
+    
+    out = str(a)
+    while a > 1:
+        x = a/b
+        if a == x*b and x% b == 0:
+            a = x
+            out = f"{out} {a}"
+        elif x == 1:
+            out += " 1"
+            break
+        else:
+            out = "Boring!"
+            break
+    print(out) 
